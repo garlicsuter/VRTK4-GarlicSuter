d603339ddbe8907461c02597d9cf4f2161c64bd0
Mark Suter <msuter@live.elida.k12.oh.us>
2021-02-25 07:56:42 -0500

    new tutorials

---
+++ Assets/Scripts/Spawnomatic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnomatic : MonoBehaviour
+{
+    public GameObject SpawnThis;
+    public string myName;
+
+    
+    public void SpawnTime()
+    {
+        //Instantiate(SpawnThis, new Vector3(i* 2.0F, 0, 0), Quaternion.identity);
+        Instantiate(SpawnThis, new Vector3(0.7f,-1.37f,2), transform.rotation);
+    }
+}
